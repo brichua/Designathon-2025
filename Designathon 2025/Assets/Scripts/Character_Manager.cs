7987b93a19eb015d94becc7576947be583f4447c
rsiddi7777 <rsiddi@ufl.edu>
2025-11-08 16:04:44 -0500

    body shifting

---
+++ Designathon 2025/Assets/Scripts/Character_Manager.cs
@@ -0,0 +1,58 @@
+using NUnit.Framework;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Character_Manager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] List<GameObject> bodyContainers = new List<GameObject>();
+
+    [SerializeField]
+    BodyPart[] bodyParts;
+
+    void Start()
+    {
+
+        foreach(BodyPart part in bodyParts)
+        {
+            part.currentSprite = 0;
+            GameObject container = this.transform.Find(part.name).gameObject;
+            bodyContainers.Add(container);
+        }
+ 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i<bodyContainers.Count; i++)
+        {
+            SpriteRenderer partRenderer = bodyContainers[i].GetComponent<SpriteRenderer>();
+            partRenderer.sprite = bodyParts[i].allSprites[bodyParts[i].currentSprite]; 
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            shiftSprite(bodyParts[0], true);
+        }
+    }
+
+    public void shiftSprite(BodyPart part, bool toRight)
+    {
+        if (toRight)
+        {
+            part.currentSprite++;
+        }
+        else
+        {
+            part.currentSprite--;
+        }
+        if(part.currentSprite < 0) 
+        { 
+            part.currentSprite = part.allSprites.Length - 1;
+        }else if(part.currentSprite >= part.allSprites.Length) 
+        {
+            part.currentSprite = 0;
+        }
+
+    }
+}
